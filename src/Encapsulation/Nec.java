60b1ff9b5a4e283e137822ec580bcfc8b85287bc
mannembhargavi <mannembhargavi@DESKTOP-RIB1OEG>
2022-11-14 16:35:30 +0530

    demo

---
+++ src/Encapsulation/Nec.java
@@ -0,0 +1,27 @@
+package Encapsulation;
+
+public class Nec {
+	private int age;
+
+	public int getAge() {
+		if(age==18) {
+			System.out.println("eligible for anything");
+		}
+		else {
+			System.out.println("not eligible");
+		}
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+		
+	}
+
+	{
+		
+	}
+	
+
+}
+
---
+++ src/Encapsulation/Test.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class Test {
+	public static void main(String[] args) {
+		Nec n=new Nec();
+		n.setAge(18);
+		n.getAge();
+	}
+
+}
